6edfc37a8b9e6dc598be9b5e356444fa96c26a68
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2023-07-09 14:15:41 +0300

    Handle JWT
    added starting code of using jwt token

--- Server/Src/modules/controller/game.controller.js
+++ Server/Src/modules/controller/game.controller.js
@@ -6,7 +6,8 @@ export const createGame =
     async (req, res) => {
         // TODO
         let { ownerId, boardId } = req.body;
-        console.log("CREATE GAME REQUEST, CREATING GAME for ", ownerId, " board:", boardId)
+        console.log("CREATE GAME REQUEST, CREATING GAME for ", ownerId, " board:", boardId,req.headers)
+        res.send("ay 7aga ")
     }
 
 
@@ -16,8 +17,8 @@ export const createGame =
 export const joinGame =
     async (req, res) => {
 
-        let { userId, gameId  } = req.body;
-        console.log("joining game of id: ", gameId, " by: " , userId)
+        let { userId, gameId } = req.body;
+        console.log("joining game of id: ", gameId, " by: ", userId)
 
     }
 
--- Server/Src/modules/controller/user.controller.js
+++ Server/Src/modules/controller/user.controller.js
@@ -1,28 +1,57 @@
 import { userSchema } from "../../models/user.js";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const createUser = async (req, res) => {
     let { userName, password } = req.body;
+    if(!userName || !password) {
+      res.status(400).send("Username and/or password missing")
+    }
 
+    const encryptedPassword =await bcrypt.hash(password, 10);
+    password = encryptedPassword
     try {
-        let users = await userSchema.create({ userName, password });
-        res.json({ message: "user created", users });
+      let users = await userSchema.create({ userName, password });
+      const payload = {
+        userName: userName,
+        password: encryptedPassword
+      }
+      const token = jwt.sign(payload,"fdsoiuhrjiuhiuegrS")
+
+
+
+      res.json({token:token, payload:payload});
     } catch (error) {
         console.log(error, "----------");
-        res.json({ message: "user already exist " });
+        res.json({ message: "error " });
     }
+
 };
 
 export const logIn = async (req, res) => {
-    let { userName, password } = req.body;
+  let { userName, password } = req.body;
+  if(!userName || !password) {
+    res.status(400).send("Username and/or password missing")
+  }
 
-    const user = await userSchema.findAll({
-        where: {
-            userName: userName,
-            password: password,
-        },
-    });
+  const encryptedPassword =await bcrypt.hash(password, 10);
+  password = encryptedPassword
+
+
+  const user = await userSchema.findAll({
+      where: {
+          userName: userName,
+          password: password,
+      },
+  });
 
     user.length && res.send({ message: "founded", user });
     !user.length && res.send({ message: "notFounded" });
+
+    const payload = {
+      userName: userName,
+      password: encryptedPassword
+    }
+    const token = jwt.sign(payload,"fdsoiuhrjiuhiuegrS")
 };
 

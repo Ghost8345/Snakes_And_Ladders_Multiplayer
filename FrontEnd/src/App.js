7ba54e825fd192f50e7d7e9d1ab65e2b4dc6638d
jomana <jomanaehab666@yahoo.com>
2023-07-10 17:07:40 +0300

    front

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,7 +3,6 @@ import Login from "./Components/Login";
 import Register from "./Components/Register";
 import Game from "./Components/Game";
 import Lobby2 from "./Components/Lobby2";
-import CreateGame from "./Components/CreateGame";
 import { Route, Routes, Navigate } from 'react-router-dom';
 import './index.css';
 import "./App.css";
@@ -18,7 +17,6 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/game" element={<Game />} />
       <Route path="/Lobby" element={<Lobby />} />
-      <Route path="/CreateGame" element={<CreateGame />} />
       <Route path="/Lobby2" element={<Lobby2 />} />
 
       <Route path="*" element={<Navigate to="/login" replace />} />

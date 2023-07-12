b3485a46292420b9e27e3849266285a40420164b
RowanEssam61 <RowanEssam61@gmail.com>
2023-07-12 10:08:16 +0300

    leaveGame

--- Server/Src/modules/routes/game.routes.js
+++ Server/Src/modules/routes/game.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 export const gameRouter = express.Router();
-import { createGame, joinGame, move, updateBoard, getAllGames } from '../controller/game.controller.js';
+import { createGame, joinGame, move, updateBoard, getAllGames,leaveGame ,deleteGame,refreshPending} from '../controller/game.controller.js';
 gameRouter.use(express.json());
 
 gameRouter.get("/", getAllGames)
@@ -8,3 +8,6 @@ gameRouter.post("/createGame", createGame);
 gameRouter.post("/joinGame", joinGame);
 gameRouter.post("/move", move)
 gameRouter.get("/updateBoard/:gameId", updateBoard)
+gameRouter.post("/leaveGame", leaveGame);
+gameRouter.post("/deleteGame", deleteGame);
+gameRouter.post("/refreshPending", refreshPending);
